55282fd5f561c1f11b9de2e5be224532d2e778b5
Lee <Lee.Cockcroft@coral.local>
2019-03-01 11:15:51 +0100

    Updates

---
+++ precache-manifest.d02b0b9c623f7b758f89bd273a77d1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "aa035329d82e4064d81d",
+    "url": "/react-todolist/static/js/main.aa035329.chunk.js"
+  },
+  {
+    "revision": "279b1470da11ebbb9455",
+    "url": "/react-todolist/static/js/1.279b1470.chunk.js"
+  },
+  {
+    "revision": "aa035329d82e4064d81d",
+    "url": "/react-todolist/static/css/main.f4b0bb0b.chunk.css"
+  },
+  {
+    "revision": "3b050aed5a327625f2e78774fb26be76",
+    "url": "/react-todolist/index.html"
+  }
+];
